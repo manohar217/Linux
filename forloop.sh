c37d0b3aabd2bfafc8748c5a210494d364a9b280
raj <raj@gmail.com>
2023-09-16 05:08:01 +0000

    Adding Files @ Sat Sep 16 05:08:01 UTC 2023

--- forloop.sh
+++ forloop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 marbles = ["red", "blue", "green", "yellow", "purple"]
 
-for marble_color in marbles:
+for marble_color in "${marbles[@]}";
 do
     echo "This marble is $marble_color"
 done
