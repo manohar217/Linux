c2e8ad41d478239e7067f54962bbcaa34c5cfaf8
raj <raj@gmail.com>
2023-09-20 14:47:48 +0000

    created folders and files Wed Sep 20 14:47:48 UTC 2023

---
+++ git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /root/Linux
+git status
+git add .
+git commit -m "created folders and files `date`"
+git push origin main
---
+++ task/script1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir folder1
+echo "folder created"
+touch file
+echo "file created"
